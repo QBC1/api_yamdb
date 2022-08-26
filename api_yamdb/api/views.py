1cb284908f1cc49c9ef24dce454d74b78e4abfd1
Николай Распопин <off_on@bk.ru>
2022-08-26 17:05:07 +0800

    Вынес функцию отправки почты в отдельный модуль, немного подчистил RequestCreateUserViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,39 +1,22 @@
 import secrets
 
-from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-
 from rest_framework import permissions, status, views, viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import User
 
+from .extra_functions import send_code_by_email
 from .permissions import AdminPermissions
-from .serializers import (CreateUserSerialise, RequestCreateUserSerialise,
-                          UsersSerializer, MeUserSerializer)
-
-
-def send_code_by_email(user):
-    username = user.username
-    code = user.confirmation_code
-    email = user.email
-    message = (
-        f'''Для регистрации на сайте пройдите по ссылке:
-            http://127.0.0.1:8000/api/v1/auth/token/
-            с параметрами username: "{username}" confirmation_code="{code}"
-            ''')
-    send_mail(
-        message=message,
-        subject="Регистрация пользователя",
-        recipient_list=[email, ],
-        from_email="registration@yamdb.ru",)
+from .serializers import (CreateUserSerialise, MeUserSerializer,
+                          RequestCreateUserSerialise, UsersSerializer)
 
 
 class RequestCreateUserViewSet(viewsets.ViewSet):
     queryset = User.objects.all()
     serializer_class = RequestCreateUserSerialise
-    http_method_names = ['post',]
+    http_method_names = ['post', ]
 
     def create(self, serializer):
         username = self.request.data.get('username')
@@ -57,26 +40,19 @@ class RequestCreateUserViewSet(viewsets.ViewSet):
 class CreateUserViewSet(viewsets.ViewSet):
     queryset = User.objects.all()
     serializer_class = CreateUserSerialise
-    http_method_names = ['post',]
+    http_method_names = ['post', ]
 
     def create(self, request):
         username = self.request.data.get('username')
         confirmation_code = self.request.data.get('confirmation_code')
 
-        if not username or not confirmation_code or username == 'me':
+        if not username or not confirmation_code:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        if not User.objects.filter(username=username).exists():
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        if not User.objects.filter(confirmation_code=confirmation_code).exists():
+        user = get_object_or_404(User, username=username)
+        if user.confirmation_code != confirmation_code:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        if not User.objects.filter(
-            username=username).filter(confirmation_code=confirmation_code).exists():
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        user = User.objects.get(username=username)
         refresh = RefreshToken.for_user(user)
         response = {
             'refresh': str(refresh),
