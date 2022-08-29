d64a68b0d9103b79d67ee3ff77877cdff5af70a2
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-08-29 23:26:50 +0300

    Правки в админке

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -6,32 +6,49 @@ from .models import Category, Comment, Genre, Review, Title, User
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'slug')
     list_display_links = ('pk',)
+    search_fields = ('name', 'slug')
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
 
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'author', 'pub_date', 'review')
     list_display_links = ('pk',)
+    search_fields = ('text', 'author')
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'slug')
     list_display_links = ('pk',)
+    search_fields = ('name', 'slug')
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
 
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'author', 'title', 'score', 'pub_date')
     list_display_links = ('pk',)
+    search_fields = ('text', 'author')
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
 
 
 class TitleAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'year', 'category', 'description')
     list_display_links = ('pk',)
+    search_fields = ('name', 'category')
+    list_filter = ('year',)
+    empty_value_display = '-пусто-'
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'username', 'first_name',
                     'last_name', 'email', 'bio', 'role')
     list_display_links = ('pk', 'username', 'first_name', 'last_name',)
+    search_fields = ('username', 'email')
+    list_filter = ('role',)
     list_editable = ('role',)
 
 
