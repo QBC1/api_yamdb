f0c31b75549ab37cb77ade6b77997980582fe116
Николай Распопин <off_on@bk.ru>
2022-09-06 21:48:10 +0800

    correct project with new reviewers notes

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+
 from api_yamdb.settings import ADMIN, MODERATOR, ROLE_CHOICES, USER
 
 
